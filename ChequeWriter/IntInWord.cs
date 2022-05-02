28612c29e0ee7eb29fcbb28fa9780e4a81608c66
Гиорги <avakov.giorgi@live.com>
2022-05-02 13:02:37 +0300

    rus\eng

---
+++ ChequeWriter/IntInWord.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChequeWriter
+{
+    internal class IntInWord
+    {
+        public IntInWord() { }
+
+        public string TranscriptEng(int num)
+        {
+            string result = string.Empty;
+
+            if (((num - (num % 100)) / 100) != 0) // проверяем имеет ли число третьий разряд
+                switch (((num - (num % 100)) / 100))
+                {
+                    case 1: result += " one hundred"; break;
+                    case 2: result += " two hundred"; break;
+                    case 3: result += " three hundred"; break;
+                    case 4: result += " four hundred"; break;
+                    case 5: result += " five hundred"; break;
+                    case 6: result += " six hundred"; break;
+                    case 7: result += " seven hundred"; break;
+                    case 8: result += " eight hundred"; break;
+                    case 9: result += " nine hundred"; break;
+                }
+            if (((num % 100) - ((num % 100) % 10)) / 10 != 1)// проверяем имеет ли число второй разряд
+            {
+                switch (((num % 100) - ((num % 100) % 10)) / 10)
+                {
+                    case 2: result += " twenty"; break;
+                    case 3: result += " thirty"; break;
+                    case 4: result += " forty"; break;
+                    case 5: result += " fifty"; break;
+                    case 6: result += " sixty"; break;
+                    case 7: result += " seventy"; break;
+                    case 8: result += " eighty"; break;
+                    case 9: result += " ninety"; break;
+                }
+                switch (num % 10)
+                {
+                    case 1: result += " one"; break;
+                    case 2: result += " two"; break;
+                    case 3: result += " three"; break;
+                    case 4: result += " four"; break;
+                    case 5: result += " five"; break;
+                    case 6: result += " six"; break;
+                    case 7: result += " seven"; break;
+                    case 8: result += " eight"; break;
+                    case 9: result += " nine"; break;
+                }
+            }
+            else switch (num % 100) // здесь проверяем второй разряд | если второй разряд >= 10 && второй разряд <= 19
+            {
+                case 10: result += " ten"; break;
+                case 11: result += " eleven"; break;
+                case 12: result += " twelve"; break;
+                case 13: result += " thirteen"; break;
+                case 14: result += " fourteen"; break;
+                case 15: result += " fifteen"; break;
+                case 16: result += " sixteen"; break;
+                case 17: result += " seventeen"; break;
+                case 18: result += " eighteen"; break;
+                case 19: result += " nineteen"; break;
+            }
+            return result;
+        }
+
+        public string TranscriptRus(int num)
+        {
+            string result = string.Empty;
+
+            if (((num - (num % 100)) / 100) != 0) // проверяем имеет ли число третьий разряд
+            {
+                switch (((num - (num % 100)) / 100))
+                {
+                    case 1: result += " сто"; break;
+                    case 2: result += " двести"; break;
+                    case 3: result += " триста"; break;
+                    case 4: result += " четыреста"; break;
+                    case 5: result += " пятьсот"; break;
+                    case 6: result += " шестьсот"; break;
+                    case 7: result += " семьсот"; break;
+                    case 8: result += " восемьсот"; break;
+                    case 9: result += " девятьсот"; break;
+                }
+            }
+            if (((num % 100) - ((num % 100) % 10)) / 10 != 1)// проверяем имеет ли число второй разряд
+            {
+                switch (((num % 100) - ((num % 100) % 10)) / 10)
+                {
+                    case 2: result += " двадцать"; break;
+                    case 3: result += " тридцать"; break;
+                    case 4: result += " сорок"; break;
+                    case 5: result += " пятьдесят"; break;
+                    case 6: result += " шестьдесят"; break;
+                    case 7: result += " семьдесят"; break;
+                    case 8: result += " восемьдесят"; break;
+                    case 9: result += " девяносто"; break;
+                }
+                switch (num % 10)
+                {
+                    case 1: result += " один"; break;//условие нужно для чисел больше миллиона
+                    case 2: result += " два"; break;
+                    case 3: result += " три"; break;
+                    case 4: result += " четыре"; break;
+                    case 5: result += " пять"; break;
+                    case 6: result += " шесть"; break;
+                    case 7: result += " семь"; break;
+                    case 8: result += " восемь"; break;
+                    case 9: result += " девять"; break;
+                }
+            }            
+            else
+                switch (num % 100) // здесь проверяем второй разряд | если второй разряд >= 10 && второй разряд <= 19
+                {
+                    case 10: result += " десять"; break;
+                    case 11: result += " одиннадцать"; break;
+                    case 12: result += " двенадцать"; break;
+                    case 13: result += " тринадцать"; break;
+                    case 14: result += " четырнадцать"; break;
+                    case 15: result += " пятнадцать"; break;
+                    case 16: result += " шестнадцать"; break;
+                    case 17: result += " семнадцать"; break;
+                    case 18: result += " восемннадцать"; break;
+                    case 19: result += " девятнадцать"; break;
+                }
+            return result;
+        }
+    }
+}
--- ChequeWriter/Program.cs
+++ ChequeWriter/Program.cs
@@ -8,16 +8,47 @@ namespace ChequeWriter
 {
     internal class Program
     {
+        public static string[,] array_string_rus = new string[4, 3] 
+        {
+            { " миллиард", " миллиарда", " миллиардов" },
+            { " миллион", " миллиона", " миллионов" },
+            { " тысяча", " тысячи", " тысяч" },
+            { "", "", "" }
+        };
+        public static string[] array_string_eng = new string[4] 
+        {
+            "billion", 
+            "million",
+            "thousand",
+            ""
+        };
         static void Main(string[] args)
         {
             int maxValue = 2000000000, dollarNum = 0, centNum = 0;
-            string result = ""; // строка для вывода результата
+            string result = string.Empty, lang = string.Empty; // строка для вывода результата
             #region Получаем значения
+            try
+            {
+                Console.WriteLine("Выберите язык вывода: rus\\eng");
+                lang = Console.ReadLine().ToLower();
+                if(!lang.Equals("rus") && !lang.Equals("eng"))
+                {
+                    throw new Exception("Выбранный язык не найден");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine("Язык по умолчанию rus");
+            }
             while (true)
             {
                 try
                 {
-                    Console.WriteLine("Enter the number of dollars");
+                    if (lang.Equals("eng"))
+                        Console.WriteLine("Enter the number of dollars");
+                    else
+                        Console.WriteLine("Введите сумму долларов");
                     dollarNum = int.Parse(Console.ReadLine()); // получаем $
                     if (dollarNum < 0 || dollarNum > maxValue)
                         throw new Exception();
@@ -25,7 +56,10 @@ namespace ChequeWriter
                 }
                 catch
                 {
-                    Console.WriteLine("Check the correctness of the entered amount");
+                    if (lang.Equals("eng"))
+                        Console.WriteLine("Check the correctness of the entered amount");
+                    else
+                        Console.WriteLine("Введено некорректное значение");
                     continue;
                 }
             }
@@ -33,7 +67,10 @@ namespace ChequeWriter
             {
                 try
                 {
-                    Console.WriteLine("Enter the number of cents");
+                    if (lang.Equals("eng"))
+                        Console.WriteLine("Enter the number of cents");
+                    else
+                        Console.WriteLine("Введите сумму центов");
                     centNum = int.Parse(Console.ReadLine());// получаем ¢
                     if (centNum < 0 || centNum > 99)
                         throw new Exception();
@@ -41,127 +78,80 @@ namespace ChequeWriter
                 }
                 catch
                 {
-                    Console.WriteLine("Check the correctness of the entered amount");
+                    if (lang.Equals("eng"))
+                        Console.WriteLine("Check the correctness of the entered amount");
+                    else
+                        Console.WriteLine("Введено некорректное значение");
                     continue;
                 }
             }
             #endregion
 
-            int[] array_int = new int[4]; // нужно для разбивания число по сотням типа 123123123 == 123 123 123 в массиве
-            string[] array_string = new string[4] {"billion", // нужно для чисел больше миллиона
-                "million",
-                "thousand",
-                ""};
-            array_int[0] = (dollarNum - (dollarNum % 1000000000)) / 1000000000; // здесь мы разбиваем наше число по элементам массива по 3 цифры
-            array_int[1] = ((dollarNum % 1000000000) - (dollarNum % 1000000)) / 1000000;// допустим мы ввели 123345, значит  array_int[3] == 345  ,  array_int[2] == 123  
-            array_int[2] = ((dollarNum % 1000000) - (dollarNum % 1000)) / 1000;
-            array_int[3] = dollarNum % 1000;
-
-            for (int i = 0; i < 4; i++)
+            int[] array_int = new int[4]; // нужно для разбивания число по сотням типа 123123123 == 123 123 123 в массиве            
+            Spliter(dollarNum, out array_int);
+            IntInWord inWord = new IntInWord();
+            for (int i = 0; i < array_int.Length; i++)
             {
                 if (array_int[i] != 0) //array_int[0] != 0 если число больше миллиарда, array_int[1] != 0 если число больше миллиона , array_int[2] != 0 если число больше сто тысяч
                 {
-                    if (((array_int[i] - (array_int[i] % 100)) / 100) != 0) // проверяем имеет ли число третьий разряд
-                        switch (((array_int[i] - (array_int[i] % 100)) / 100))
-                        {
-                            case 1: result += " one hundred"; break;
-                            case 2: result += " two hundred"; break;
-                            case 3: result += " three hundred"; break;
-                            case 4: result += " four hundred"; break;
-                            case 5: result += " five hundred"; break;
-                            case 6: result += " six hundred"; break;
-                            case 7: result += " seven hundred"; break;
-                            case 8: result += " eight hundred"; break;
-                            case 9: result += " nine hundred"; break;
-                        }
-                    if (((array_int[i] % 100) - ((array_int[i] % 100) % 10)) / 10 != 1)// проверяем имеет ли число второй разряд
+                    if (lang.Equals("eng"))
                     {
-                        switch (((array_int[i] % 100) - ((array_int[i] % 100) % 10)) / 10)
-                        {
-                            case 2: result += " twenty"; break;
-                            case 3: result += " thirty"; break;
-                            case 4: result += " forty"; break;
-                            case 5: result += " fifty"; break;
-                            case 6: result += " sixty"; break;
-                            case 7: result += " seventy"; break;
-                            case 8: result += " eighty"; break;
-                            case 9: result += " ninety"; break;
-                        }
-                        switch (array_int[i] % 10)
-                        {
-                            case 1: result += " one"; break;
-                            case 2: result += " two"; break;
-                            case 3: result += " three"; break;
-                            case 4: result += " four"; break;
-                            case 5: result += " five"; break;
-                            case 6: result += " six"; break;
-                            case 7: result += " seven"; break;
-                            case 8: result += " eight"; break;
-                            case 9: result += " nine"; break;
-                        }
-                    }                    
-                    else switch (array_int[i] % 100) // здесь проверяем второй разряд | если второй разряд >= 10 && второй разряд <= 19
-                    {
-                        case 10: result += " ten"; break;
-                        case 11: result += " eleven"; break;
-                        case 12: result += " twelve"; break;
-                        case 13: result += " thirteen"; break;
-                        case 14: result += " fourteen"; break;
-                        case 15: result += " fifteen"; break;
-                        case 16: result += " sixteen"; break;
-                        case 17: result += " seventeen"; break;
-                        case 18: result += " eighteen"; break;
-                        case 19: result += " nineteen"; break;
+                        result += inWord.TranscriptEng(array_int[i]);
+                        result += " " + array_string_eng[i];
                     }
-                    result += " " + array_string[i] + " dollars ";
-                }
+                    else
+                    {
+                        result += inWord.TranscriptRus(array_int[i]);
+                        if (array_int[i] % 100 >= 10 && array_int[i] % 100 <= 19) result += " " + array_string_rus[i, 2] + " ";
+                        else 
+                            switch (array_int[i] % 10)
+                            {
+                                case 1: result += " " + array_string_rus[i, 0] + " "; break; // 1 тысяча, 1 миллион, 1 миллиард
+                                case 2:
+                                case 3:
+                                case 4: result += " " + array_string_rus[i, 1] + " "; break;// 3 тысячи , 3 миллиона, 3 миллиарда
+                                case 5:
+                                case 6:
+                                case 7:
+                                case 8:
+                                case 9: result += " " + array_string_rus[i, 2] + " "; break;// 9 тысяч , 9 миллионов, 9 миллиардов
+                            }
+                    }
+                    
+                }                
+            }
+            if (dollarNum != 0)
+            {
+                if (lang.Equals("eng"))
+                    result += "dollars";
+                else
+                    result += "доллара";
             }
             // перевод в текст центов
             if (centNum != 0)
             {
-                if (((centNum % 100) - ((centNum % 100) % 10)) / 10 != 1)// проверяем имеет ли число второй разряд
+                if (lang.Equals("eng"))
                 {
-                    switch (((centNum % 100) - ((centNum % 100) % 10)) / 10)
-                    {
-                        case 2: result += " twenty"; break;
-                        case 3: result += " thirty"; break;
-                        case 4: result += " forty"; break;
-                        case 5: result += " fifty"; break;
-                        case 6: result += " sixty"; break;
-                        case 7: result += " seventy"; break;
-                        case 8: result += " eighty"; break;
-                        case 9: result += " ninety"; break;
-                    }
-                    switch (centNum % 10)
-                    {
-                        case 1: result += " one"; break;
-                        case 2: result += " two"; break;
-                        case 3: result += " three"; break;
-                        case 4: result += " four"; break;
-                        case 5: result += " five"; break;
-                        case 6: result += " six"; break;
-                        case 7: result += " seven"; break;
-                        case 8: result += " eight"; break;
-                        case 9: result += " nine"; break;
-                    }
+                    result += inWord.TranscriptEng(centNum);
+                    result += " cents";
                 }
-                else switch (centNum % 100) // здесь проверяем второй разряд | если второй разряд >= 10 && второй разряд <= 19
+                else
                 {
-                    case 10: result += " ten"; break;
-                    case 11: result += " eleven"; break;
-                    case 12: result += " twelve"; break;
-                    case 13: result += " thirteen"; break;
-                    case 14: result += " fourteen"; break;
-                    case 15: result += " fifteen"; break;
-                    case 16: result += " sixteen"; break;
-                    case 17: result += " seventeen"; break;
-                    case 18: result += " eighteen"; break;
-                    case 19: result += " nineteen"; break;
+                    result+=inWord.TranscriptRus(centNum);
+                    result += "цента";
                 }
-                result += " cents";
             }            
             Console.WriteLine(result.Trim());
             Console.ReadKey();
+        }
+
+        static void Spliter(int dollarNum, out int[] outArray)
+        {
+            outArray = new int[4];
+            outArray[0] = (dollarNum - (dollarNum % 1000000000)) / 1000000000; // здесь мы разбиваем наше число по элементам массива по 3 цифры
+            outArray[1] = ((dollarNum % 1000000000) - (dollarNum % 1000000)) / 1000000;// допустим мы ввели 123345, значит  array_int[3] == 345  ,  array_int[2] == 123  
+            outArray[2] = ((dollarNum % 1000000) - (dollarNum % 1000)) / 1000;
+            outArray[3] = dollarNum % 1000;
         }
     }
 }
